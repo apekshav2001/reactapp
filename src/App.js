9fc5343c62d48c1af783b8990097bfa741f8e644
Apeksha <apeksha2001verma@gmail.com>
2023-04-28 19:44:11 +0530

    Add new Item

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 
 
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
 
 function App()  {    
-  const [items , setItems] = useState([
-    {
-        id:1,
-        checked:false,
-        item:"item1",
-    },
-    {
-        id:2,
-        checked:false,
-        item:"item2",
-    },
-    {
-        id:3,
-        checked:false,
-        item:"item3",
-    }
-]);
-const handleCheck =(id) =>{
+  const [items , setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+const [newItem, setNewItem] = useState(' ')
+
+const setAndSaveItems = (newItems) => {
+  setItems(newItems);
+  localStorage.setItem('shoppinglist' ,JSON.stringify(newItems));
+}
+
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length -1].id + 1 : 1;
+  const myNewItem ={id, checked:false, item};
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
+}
+const handleCheck = (id) =>{
   const listItems = items.map((item) => item.id === id ? {...item,checked: !item.checked } : item);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist' ,JSON.stringify(listItems));
+   setAndSaveItems(listItems);
+
 }
  const handleDelete =(id) => {
   const listItems = items.filter((item) => item.id !== id);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist' ,JSON.stringify(listItems));
+   setAndSaveItems(listItems);
+
+ }
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  if(!newItem) return;
+  console.log(newItem);
+  addItem(newItem)
+  setNewItem('');
+  
  }
   return ( 
     <div className="App">
       <Header title= "Grocery List"/>
+      <AddItem
+      newItem={newItem}
+      setNewItem={setNewItem}
+      handleSubmit={handleSubmit}
+      />
       <Content   
         items={items} 
         handleCheck={handleCheck}
